969a9e57967ac8e93faa18098c6953a737ac605d
Fabio Matricardi <fabio.matricardi@gmail.com>
2025-05-04 21:54:36 +0800

    Add files via upload

---
+++ install.bat
@@ -0,0 +1,14 @@
+echo off
+echo Downloading the Language model...
+wget https://hf-mirror.com/bartowski/Qwen_Qwen3-4B-GGUF/resolve/main/Qwen_Qwen3-4B-Q4_K_L.gguf -nv --show-progress
+echo Unzipping the llama.cpp binaries...
+tar -xf llama-b5273-bin-win-vulkan-x64.zip
+echo Creating Virtual environment
+python -m venv venv
+echo Activating venv
+call .\venv\Scripts\activate.bat
+echo Installing dependencies
+pip install easygui tiktoken pypdf rich openai
+start cmd.exe /c llama-server.exe -m Qwen_Qwen3-4B-Q4_K_L.gguf -c 32768 -ngl 999
+python QWEN3-4B-it.py
+PAUSE
